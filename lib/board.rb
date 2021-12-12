0649933758a5c78c5506e22c7caed337ba4a63f5
rungmc <gregmcm@gmail.com>
2021-12-11 23:50:04 -0500

    Basic game board initiation and operability added

--- lib/board.rb
+++ lib/board.rb
@@ -2,69 +2,31 @@
 
 # Establishes the chess board, stores all location data.
 class Board
-  attr_accessor :grid, :history
+  attr_accessor :board, :history
 
-  def initialize(grid = fresh_board, history = [])
-    @grid = grid
+  def initialize(board = fresh_board, history = [])
+    @board = board
     @history = history
   end
 
   def fresh_board
-    # Format: grid[x][y]
-    @grid = Array.new(8, Array.new(8))
-    place_pawns
-    place_rooks
-    place_knights
-    place_bishops
-    place_kings
-    place_queens
-  end
-
-  def place_pawns
-    @grid.each do |row|
-      row[1] = Pawn.new('white')
-      row[6] = Pawn.new('black')
-    end
-  end
-
-  def place_rooks
-    @grid[0][0] = Rook.new('white')
-    @grid[7][0] = Rook.new('white')
-    @grid[0][7] = Rook.new('black')
-    @grid[7][7] = Rook.new('black')
-  end
-
-  def place_knights
-    @grid[1][0] = Knight.new('white')
-    @grid[6][0] = Knight.new('white')
-    @grid[1][7] = Knight.new('black')
-    @grid[6][7] = Knight.new('black')
-  end
-
-  def place_bishops
-    @grid[2][0] = Bishop.new('white')
-    @grid[5][0] = Bishop.new('white')
-    @grid[2][7] = Bishop.new('black')
-    @grid[5][7] = Bishop.new('black')
-  end
-
-  def place_kings
-    @grid[4][0] = King.new('white')
-    @grid[4][7] = King.new('black')
-  end
-
-  def place_queens
-    @grid[3][0] = Queen.new('white')
-    @grid[3][7] = Queen.new('black')
+    # Format: board[x][y]
+    board = Array.new(8) { Array.new(8) }
+    board = place_pawns(board)
+    board = place_rooks(board)
+    board = place_knights(board)
+    board = place_bishops(board)
+    board = place_queens(board)
+    place_kings(board)
   end
 
   # Executes a move (no checks for validity) and adds it to the board history.
   def move(piece, start, destination)
-    @grid[start[0]][start[1]] = nil
-    @grid[destination[0]][destination[1]] = piece
+    @board[start[0]][start[1]] = nil
+    @board[destination[0]][destination[1]] = piece
     piece.moved = true
 
-    @history << "#{piece.symbol}#{readable(start)}#{note_capture(destination)}#{readable(dest)}"
+    @history << "#{piece.symbol}#{readable(start)}#{check_capture(destination)}#{readable(dest)}"
   end
 
   # Translates internally used coordinates to chess notation.
@@ -73,7 +35,52 @@ class Board
   end
 
   # Denotes whether or not a capture has occurred by executing a move.
-  def note_capture(coord)
-    @grid[coord[0]][coord[1]].nil? ? '-' : "x#{@grid[coord[0]][coord[1]].symbol}"
+  def check_capture(coord)
+    @board[coord[0]][coord[1]].nil? ? '-' : "x#{@board[coord[0]][coord[1]].symbol}"
+  end
+
+  private
+
+  def place_pawns(board)
+    board.each do |row|
+      row[1] = Pawn.new('white', 'P')
+      row[6] = Pawn.new('black', 'P')
+    end
+  end
+
+  def place_rooks(board)
+    board[0][0] = Rook.new('white', 'R')
+    board[7][0] = Rook.new('white', 'R')
+    board[0][7] = Rook.new('black', 'R')
+    board[7][7] = Rook.new('black', 'R')
+    board
+  end
+
+  def place_knights(board)
+    board[1][0] = Knight.new('white', 'N')
+    board[6][0] = Knight.new('white', 'N')
+    board[1][7] = Knight.new('black', 'N')
+    board[6][7] = Knight.new('black', 'N')
+    board
+  end
+
+  def place_bishops(board)
+    board[2][0] = Bishop.new('white', 'B')
+    board[5][0] = Bishop.new('white', 'B')
+    board[2][7] = Bishop.new('black', 'B')
+    board[5][7] = Bishop.new('black', 'B')
+    board
+  end
+
+  def place_queens(board)
+    board[3][0] = Queen.new('white', 'Q')
+    board[3][7] = Queen.new('black', 'Q')
+    board
+  end
+
+  def place_kings(board)
+    board[4][0] = King.new('white', 'K')
+    board[4][7] = King.new('black', 'K')
+    board
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,31 +1,47 @@
 # frozen_string_literal: true
 
+require_relative '../lib/board'
+require_relative '../lib/pieces/chess_piece'
+require_relative '../lib/pieces/pawn'
+require_relative '../lib/pieces/knight'
+require_relative '../lib/pieces/bishop'
+require_relative '../lib/pieces/rook'
+require_relative '../lib/pieces/queen'
+require_relative '../lib/pieces/king'
+
 describe Board do
   subject(:board) { described_class.new }
-  
-  describe '#initialize' do
-  end
 
-  describe '#update' do
-    it '' do
-    end
+  describe '#fresh_board' do
+    context 'when starting a new game' do
+      it 'creates a row of white pawns at A2-H2/(0..7, 1)' do
+        board.board.each { |i| expect(i[1].is_a?(Pawn)).to be true }
+      end
 
-    it '' do
+      it 'creates a row of black pawns at A7-H7/(0..7, 6)' do
+        board.board.each { |i| expect(i[6].is_a?(Pawn)).to be true }
+      end
     end
   end
 
   describe '#readable' do
-    it ''
+    context 'when given a set of x,y coordinates' do
+      it 'translates to chess notation' do
+        expect(board.readable([3,4])).to eq("D5")
+      end
+    end
   end
 
-  describe '#note_capture' do
-    context 'when the space being moved to is nil' do
+  describe '#check_capture' do
+    context 'when the space being moved to has nothing on it' do
       it 'returns -' do
+        expect(board.check_capture([3,3])).to eq('-')
       end
     end
 
     context 'when there is already a piece in the space' do
       it 'returns x with the symbol of the captured piece' do
+        expect(board.check_capture([1,0])).to eq("xN")
       end
     end
   end
