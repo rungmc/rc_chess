d8ff6bf4d57652da428e32f8101f961271c374aa
rungmc <gregmcm@gmail.com>
2021-12-13 13:33:38 -0500

    Outlined the basic game loop

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
-class Chess
+require_relative 'board'
+
+class Game
+  def initialize(@board = Board.new, white_turn: true)
+    @board = board
+    @white_turn = white_turn
+  end
+
+  def play
+    loop do
+      player_input
+      update_board
+      break if @board.checkmate?
+      change_turns
+    end
+    declare_winner
+    enable_history # opt. display method
+  end
 end
